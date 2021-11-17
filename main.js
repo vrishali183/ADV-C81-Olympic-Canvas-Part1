394c65ade6db8dea3ab7e9ffb8ff87346a0175d3
vrishali183 <vrishali183@gmail.com>
2021-11-17 05:35:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,35 +4,41 @@ ctx= canvas.getContext("2d");
 ctx.beginPath();
 ctx.strokeStyle = "grey";
 ctx.lineWidth = 1;
-ctx.rect(150, 143, 430, 200);
+//x Axis 150
+ctx.rect(, 143, 430, 200);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "blue";
 ctx.lineWidth = 5;
-ctx.arc(250, 210, 40 ,0 , 2 * Math.PI);
+//x Axis 250
+ctx.arc(, 210, 40 ,0 , 2 * Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "black";
 ctx.lineWidth = 5;
-ctx.arc(350, 210, 40 ,0 , 2 * Math.PI);
+//x Axis 350
+ctx.arc(, 210, 40 ,0 , 2 * Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "red";
 ctx.lineWidth = 5;
-ctx.arc(450, 210, 40 ,0 , 2 * Math.PI);
+//x Axis 450
+ctx.arc(, 210, 40 ,0 , 2 * Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "orange";
 ctx.lineWidth = 5;
-ctx.arc(300, 250, 40 ,0 , 2 * Math.PI);
+//x Axis 300
+ctx.arc(, 250, 40 ,0 , 2 * Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "green";
 ctx.lineWidth = 5;
-ctx.arc(400, 250, 40 ,0 , 2 * Math.PI);
+//x Axis 400
+ctx.arc(, 250, 40 ,0 , 2 * Math.PI);
 ctx.stroke();
